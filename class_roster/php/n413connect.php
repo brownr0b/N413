8a826cdccaab2ad3e6627fce359f0c3f39e438b4
Robert Brown <32443336+brownr0b@users.noreply.github.com>
2018-01-25 16:30:27 -0500

    Updated n413connect.php
    Changed user/pwd from root to brownrob for live version

--- class_roster/php/n413connect.php
+++ class_roster/php/n413connect.php
@@ -2,9 +2,9 @@
 ////////////////////// MAMP version ///////////////////////
 
 	$dbhost = 'localhost';
-	$dbuser = 'root';
-	$dbpwd  = 'root';
-	$dbname = 'n413_class_roster';  //'<your MAMP database name>';
+	$dbuser = 'brownrob';
+	$dbpwd  = 'brownrob';
+	$dbname = 'brownrob_db';  //'<your MAMP database name>';
 
 	$link = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
 
