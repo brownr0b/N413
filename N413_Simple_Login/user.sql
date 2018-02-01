16d506e6b1e5f6643ffcf56013273ac276b58627
Robert Brown <32443336+brownr0b@users.noreply.github.com>
2018-02-01 16:00:39 -0500

    Add files via upload

--- N413_Simple_Login/user.sql
+++ N413_Simple_Login/user.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 01, 2018 at 09:57 AM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Host: localhost
+-- Generation Time: Feb 01, 2018 at 03:52 PM
+-- Server version: 10.2.12-MariaDB-10.2.12+maria~xenial
+-- PHP Version: 7.0.22-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `n413_simple_login`
+-- Database: `brownrob_db`
 --
 
 -- --------------------------------------------------------
@@ -28,10 +28,19 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `user` (
   `ID` int(11) NOT NULL,
-  `Username` varchar(25) NOT NULL,
-  `Email` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL
+  `username` varchar(25) NOT NULL,
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`ID`, `username`, `password`) VALUES
+(6, 'Test', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'),
+(8, 'brownrob', '854d99ee740e074418756cc5b7eb1841b0d1cd99'),
+(9, 'brjebrow', 'c1da0c8f948572478adc8bf22e01e6c10c3a81cd'),
+(10, 'test2', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3');
 
 --
 -- Indexes for dumped tables
@@ -51,7 +60,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
